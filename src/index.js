a1fd3779a83553392bf4e014226b5a8641a06808
izuru775 <izuru775@gmail.com>
2021-12-01 22:30:05 +1100

    Minor change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
     <Router>
-        Created by : Ish Adikaramm (Deakin University)
+        Created by : Ish Adikaram (Deakin University)
         <App/>
     </Router>,
     document.getElementById("root")
